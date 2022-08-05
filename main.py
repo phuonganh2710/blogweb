83d910fa65283fe4621451f3652ab6db5ba0214b
Phương Anh Đặng <phuonganhdang271000@gmail.com>
2022-08-06 00:38:28 +0900

    Change sql

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from functools import wraps
-
+import os
 
 from flask_gravatar import Gravatar
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -32,7 +32,7 @@ gravatar = Gravatar(app,
                     base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -69,7 +69,7 @@ class Comment(db.Model):
     comment_post_id = db.Column(db.Integer, db.ForeignKey('blog_posts.id'))
 
 
-#db.create_all()
+db.create_all()
 
 
 def admin_only(f):
